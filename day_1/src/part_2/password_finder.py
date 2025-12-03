c0d344ec7f43fd7613d88e997d3dbc4fc8b114f5
João Vitor de Camargo <camargo.dev@gmail.com>
2025-12-02 21:47:46 -0300

    Part 2 done

--- day_1/src/part_2/password_finder.py
+++ day_1/src/part_2/password_finder.py
@@ -10,25 +10,23 @@ class PasswordFinder:
         count_for_zeroes = 0
         for line in lines:
             side, total_num_clicks = line[0], int(line[1:])
-            print(position, side, total_num_clicks)
-            # num_of_full_rotations = total_num_clicks // self.NUM_SLOTS
+            num_of_full_rotations = total_num_clicks // self.NUM_SLOTS
+            remaining_number_of_clicks = total_num_clicks % self.NUM_SLOTS
 
-            passed_through_zero = (side == self.LEFT and total_num_clicks >= position) or (side == self.RIGHT and (total_num_clicks + position) >= self.NUM_SLOTS)
+            passed_through_zero = (side == self.LEFT and remaining_number_of_clicks >= position) or (side == self.RIGHT and (remaining_number_of_clicks + position) >= self.NUM_SLOTS)
 
-            # count_for_zeroes += num_of_full_rotations
+            count_for_zeroes += num_of_full_rotations
             if (position != 0 and passed_through_zero):
-                print("+1")
                 count_for_zeroes += 1
 
             if (side == self.LEFT):
-                position -= total_num_clicks
+                position -= remaining_number_of_clicks
             else:
-                position += total_num_clicks
+                position += remaining_number_of_clicks
 
             if position < 0:
                 position = self.NUM_SLOTS + position
 
             position = position % self.NUM_SLOTS
-            print()
 
         return count_for_zeroes
