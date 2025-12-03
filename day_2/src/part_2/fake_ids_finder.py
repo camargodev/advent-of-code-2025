163dc1501024c2374ad2d5ea434d75ed00f8c89a
João Vitor de Camargo <camargo.dev@gmail.com>
2025-12-02 22:49:58 -0300

    Part 2

--- day_2/src/part_2/fake_ids_finder.py
+++ day_2/src/part_2/fake_ids_finder.py
@@ -1,3 +1,22 @@
 class FakeIdsFinder:
     def find(self, lines):
-        pass
+        ranges = self.extract_ranges(lines)
+        sum_of_invalid_ids = 0
+        for (start, end) in ranges:
+            for number in range(start, end + 1):
+                if self.is_invalid(number):
+                    sum_of_invalid_ids += number
+        return sum_of_invalid_ids
+
+    def extract_ranges(self, lines):
+        pairs = []
+        for line in lines:
+            for part in line.split(","):
+                a, b = part.split("-")
+                pairs.append((int(a), int(b)))
+        return pairs
+    
+    def is_invalid(self, number):
+        string_number = str(number)
+        # string + string contém s no meio se for repetição
+        return string_number in (string_number + string_number)[1:-1]
